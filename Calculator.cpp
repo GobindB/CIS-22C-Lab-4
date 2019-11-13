8fe5f71eb975b10fd23408fcda476b9c90e3b714
Gobind Bakhshi <45723248+GobindB@users.noreply.github.com>
2019-11-12 22:24:06 -0800

    Add files via upload

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,24 +9,40 @@
 #include "Calculator.h"
 
 
-int Calculator::calculate(const std::string &inputExpression)
+/** validates the expression syntax and pushes operators/operands to member stacks
+ @pre expressionInfix validation
+ @post tokenize string and push operators/operands to appropriate stacks
+ */
+void Calculator::calculatePost(const std::string &inputExpression)
 {
+    operandStack.emptyList();
+    operatorStack.emptyList();
+    
+    if(validate(inputExpression))
+    {
+        infixExp = inputExpression;
+        infixToPostfix();
+        result = evaluate(postfixExp);
+    }
+    
+}
+
+/** validates the expression syntax and pushes operators/operands to member stacks
+ @pre expressionInfix validation
+ @post tokenize string and push operators/operands to appropriate stacks
+ */
+void Calculator::calculatePre(const std::string &inputExpression)
+{
+    operandStack.emptyList();
+    operatorStack.emptyList();
+    
     if(validate(inputExpression))
     {
         //set infix expression to input after validation checking
         infixExp = inputExpression;
-        
-        // clear previous result of postfix expression
-        postfixExp = "";
-        
-        
-        return result;
+        infixToPrefix();
+        result = evaluate(prefixExp);
     }
-    else
-    {
-        throw "Invalid syntax.";
-    }
-    
     
 }
 
@@ -35,10 +51,9 @@ int Calculator::calculate(const std::string &inputExpression)
  @post tokenize string and push operators/operands to appropriate stacks
  @return error message. or true if valid
  */
-bool Calculator::validate(const std::string &inputExpression)
+bool Calculator::validate(std::string inputExpression)
 {
-    Tokenizer input(inputExpression);
-    Queue<std::string> tokenQueue;
+    
     
     std::string token;
     int i = 0;
@@ -47,38 +62,36 @@ bool Calculator::validate(const std::string &inputExpression)
     int openParen = 0;
     int closedParen = 0;
     
-    // returns a queue of tokenized string
-    tokenQueue = input.split();
-    
     // must test
     // 1. series cant end with operator
-    
-    token = tokenQueue.peek();
+
+    tokenize(inputExpression);
     
     // checks to make sure first entry in input isnt an operator
-    if(isOperator(token[0]))
+    if(isOperator(tokenQueue.peek()))
     {
         throw "Invalid Input: " + inputExpression;
     }
     
     // loop through each element of the queue and determine if operator or operand while not empty
-    while( !tokenQueue.isEmpty() )
+    while(!tokenQueue.isEmpty())
     {
         operators = 0;
         operands =  0;
+        i = 0;
         
         token = tokenQueue.peek(); // test to ensure correct value is returned after peek!!!!!!!
         
         // check if token is valid and if so push to appropriate stack
-        while(token[i])
+        while(token[i] != '\0')
         {
             // check if token is an operand
             if(token[i] >= '0' && token[i] <= '9')
             {
                 operands++;
             }
-            // check if token has an operator
-            else if (isOperator(token[i]))
+            // check if token has an operator or parenthesis
+            else if (isOperator(token) || isParen(token))
             {
                 if(token[i] == '(')
                 {
@@ -96,38 +109,216 @@ bool Calculator::validate(const std::string &inputExpression)
                 throw "Invalid Input: " + inputExpression;
             }
             
+            // test to ensure token does not contain operator and operand i.e appropriate spacing has been used
+            if(operators > 0 && operands > 0)
+            {
+                throw "Invalid Input: " + inputExpression;
+            }
+            
             i++;
         }
-        // test to ensure token does not contain operator and operand i.e appropriate spacing has been used
-        if(operators > 0 && operands > 0)
-        {
-            throw "Invalid Input: " + inputExpression;
-        }
-        else if(openParen != closedParen)
-        {
-            throw "Invalid Input: " + inputExpression;
-        }
-        else if (operands > 0)
-        {
-            operandStack.push(stoi(token));
-        }
+      
         //token cant have multiple operators
-        else if(operators > 1)
+        if(operators > 1)
         {
             throw "Invalid Input: " + inputExpression;
-        }
-        else
-        {
-            // if valid operand then there will only be one member of the character token array
-            operatorStack.push(token[0]);
         }
         
         // deque this token from the queue of tokens
         tokenQueue.dequeue();
     }
-    
+    if(openParen != closedParen)
+    {
+        throw "Invalid Input: " + inputExpression;
+    }
     // if no exception has been thrown then input is valid
+    infixExp = inputExpression;
     return true;
+}
+
+/** converts expressionInfixQ Queue to expressionPostQ Queue
+ @pre expressionInfixQ checks
+ @post expressionPostQ is populated
+ */
+void Calculator::infixToPostfix()
+{
+    tokenize(infixExp);
+    
+    std::string token;
+    postfixExp = "";
+    
+
+    int i = 0;
+    
+    while(!tokenQueue.isEmpty())
+    {
+        token = tokenQueue.peek();
+        // If character is operator, pop two elements from stack, perform operation and push the result back.
+        if(isOperator(token))
+        {
+            while(!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedence(operatorStack.peek()) > precedence(infixExp[i]))
+            {
+                postfixExp += operatorStack.peek();
+                operatorStack.pop();
+            }
+            operatorStack.push(token[0]);
+        }
+        // Else if character is an operand
+        else if(isOperand(token))
+        {
+            postfixExp += token;
+            operandStack.push(std::stoi(token));
+        }
+        
+        else if (token[0] == '(')
+        {
+            operatorStack.push(token[0]);
+        }
+        else if(token[0] == ')')
+        {
+            while(!operatorStack.isEmpty() && operatorStack.peek() !=  '(')
+            {
+                postfixExp += operatorStack.peek();
+                operatorStack.pop();
+            }
+            operatorStack.pop();
+        }
+        tokenQueue.dequeue();
+    }
+    
+    while(!operatorStack.isEmpty())
+    {
+        postfixExp += operatorStack.top();
+        operatorStack.pop();
+    }
+    
+}
+
+/** converts expressionInfixQ Queue to expressionPostQ Queue
+ @pre expressionInfixQ checks
+ @post expressionPostQ is populated
+*/
+void Calculator::infixToPrefix()
+{
+    tokenize(infixExp);
+    
+    std::string token;
+    prefixExp = "";
+    
+    int i = 0;
+    
+    while(!tokenQueue.isEmpty())
+    {
+        token = tokenQueue.peek();
+        // Else if character is an operand
+        if(isOperand(token))
+        {
+            prefixExp += token;
+            operandStack.push(std::stoi(token));
+        }
+        
+        // If character is operator, pop two elements from stack, perform operation and push the result back.
+        else if(isOperator(token))
+        {
+            while(!operatorStack.isEmpty() && operatorStack.peek() != '(' && precedence(operatorStack.peek()) > precedence(infixExp[i]))
+            {
+                prefixExp += operatorStack.peek();
+                operatorStack.pop();
+            }
+            operatorStack.push(token[0]);
+        }
+        
+        else if (token[0] == '(')
+        {
+            operatorStack.push(token[0]);
+        }
+        else if(token[0] == ')')
+        {
+            while(!operatorStack.isEmpty() && operatorStack.peek() !=  '(') {
+                prefixExp += operatorStack.peek();
+                operatorStack.pop();
+            }
+            operatorStack.pop();
+        }
+        tokenQueue.dequeue();
+    }
+    
+    while(!operatorStack.isEmpty())
+    {
+        prefixExp += operatorStack.peek();
+        operatorStack.pop();
+    }
+    infixExp = reverse(infixExp);
+    infixToPostfix();
+    prefixExp = reverse(postfixExp);
+}
+
+/** check if character is a parenthesis
+ @pre None.
+ @post None.
+ @param expression the string to evaluate
+ @return true if is parenthesis, false otherwise
+ */
+int Calculator::evaluate(std::string expression)
+{
+    std::string temp;
+    
+    for(int i = 0;i < expression.length();i++)
+    {
+        
+       temp[0] = expression[i]; // BUG what if double digit
+        
+        // If character is operator, pop two elements from stack, perform operation and push the result back.
+        if(isOperator(temp))
+        {
+            // Pop two operands.
+            int operand2 = operandStack.peek();
+            operandStack.pop();
+            int operand1 = operandStack.peek();
+            operandStack.pop();
+            // Perform operation
+            int result = performOperation(expression[i], operand1, operand2);
+            //Push back result of operation on stack.
+            operandStack.push(result);
+        }
+
+    }
+    // If expression is in correct format, Stack will finally have one element. This will be the output.
+    return operandStack.peek();
+}
+
+/** converts expressionInfixQ Queue to expressionPostQ Queue
+ @pre expressionInfixQ checks
+ @post expressionPostQ is populated
+ @return expressionPostQ as a string */
+bool Calculator::isOperand(std::string C)
+{
+    bool flag = false;
+    
+    for(int i = 0; i < C.length(); i++)
+    {
+        if(C[i] >= '0' && C[0] <= '9')
+        {
+            flag = true;
+        }
+        else return false;
+    }
+    return flag;
+}
+
+/** converts expressionInfixQ Queue to expressionPostQ Queue
+ @pre expressionInfixQ checks
+ @post expressionPostQ is populated
+ @return expressionPostQ as a string */
+int Calculator::performOperation(char operation, int operand1, int operand2)
+{
+    if(operation == '+') return operand1 +operand2;
+    else if(operation == '-') return operand1 - operand2;
+    else if(operation == '*') return operand1 * operand2;
+    else if(operation == '/') return operand1 / operand2;
+    
+    else throw "Invalid operation.";
+    return -1;
 }
 
 /** returns the precedence "weight" for an operator
@@ -136,17 +327,32 @@ bool Calculator::validate(const std::string &inputExpression)
  @param op the operator
  @return Weight of the operator and -1 if not found
  */
-int Calculator::precedence(std::string op)
+int Calculator::precedence(char op)
 {
-    int weight = -1;
-    if (op == "+") weight = 1;
-    else if (op == "-") weight = 1;
-    else if (op == "*") weight = 2;
-    else if (op == "/") weight = 2;
-    else if (op == "%") weight = 2;
-    else if (op == "$") weight = 3;
-    else if (op == "^") weight = 3;
-    return weight;
+    if(op == '^')
+        return 3;
+    else if(op == '*' || op == '/')
+        return 2;
+    else if(op == '+' || op == '-')
+        return 1;
+    else
+        return -1;
+}
+
+/** check if character is a parenthesis
+ @pre None.
+ @post None.
+ @param op the character to test
+ @return true if is parenthesis, false otherwise
+ */
+bool Calculator::isParen(std::string op)
+{
+   
+    
+    if(op[0] == '(' || op[0] == ')')
+    {
+        return true;
+    }else return false;
 }
 
 /** check if character is an operator
@@ -155,11 +361,76 @@ int Calculator::precedence(std::string op)
  @param op the operator to test
  @return true if is defined operator, false if not defined operator
  */
-bool Calculator::isOperator(char op)
+bool Calculator::isOperator(std::string op)
 {
-    if (op == '-' || op == '+' || op == '/' || op == '%' || op == '/' || op == '(' || op == ')')
+    if (op[0] == '-' || op[0] == '+' || op[0] == '/' || op[0] == '%' || op[0] == '/' || op[0] =='*' )
     {
         return true;
     }
     return false;
+}
+
+/** check if character is an operator
+ @pre None.
+ @post None.
+ @return true if is defined operator, false if not defined operator
+ */
+int Calculator::getResult() const
+{
+    return result;
+}
+
+/** check if character is an operator
+ @pre None.
+ @post None.
+ @return true if is defined operator, false if not defined operator
+ */
+std::string Calculator::getPostfix() const
+{
+    return postfixExp;
+}
+
+/** check if character is an operator
+ @pre None.
+ @post None.
+ @return true if is defined operator, false if not defined operator
+ */
+std::string Calculator::getPrefix() const
+{
+    return prefixExp;
+}
+
+void Calculator::tokenize(std::string str)
+{
+    std::string temp = "";
+    
+    char temp2[] = " ";
+    
+    for(int i = 0; i < str.length()+1; i++)
+    {
+        temp2[0] = str[i];
+        
+        if(std::strncmp(temp2, " ", 1) == 0)
+        {
+            tokenQueue.enqueue(temp);
+            temp = "";
+        }
+        else temp += str[i];
+    
+    }
+    // pops out null character in the string
+    temp.pop_back();
+    tokenQueue.enqueue(temp);
+}
+
+std::string Calculator::reverse(std::string str)
+{
+    int n = static_cast<int>(str.length());
+    
+    // Swap character starting from two
+    // corners
+    for (int i = 0; i < n / 2; i++)
+        std::swap(str[i], str[n - i - 1]);
+    return str;
+    
 }
